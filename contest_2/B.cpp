8d5530d37a38af19f90456e53d88437e9e3ac5ea
AbylayDalabay <dalabayabilay@gmail.com>
2024-10-20 14:38:49 +0500

    review version

--- contest_2/B.cpp
+++ contest_2/B.cpp
@@ -5,14 +5,6 @@
 #include <random>
 #include <vector>
 
-template <typename Iterator>
-void Print(Iterator begin, Iterator end) {
-    for (Iterator current_it = begin; current_it != end; ++current_it) {
-        std::cout << *current_it << ' ';
-    }
-    std::cout << std::endl;
-}
-
 struct Player {
     int64_t score;
     int index;
@@ -23,13 +15,15 @@ struct Player {
     static bool CompByIndex(const Player& lhs, const Player& rhs) {
         return lhs.index < rhs.index;
     }
-
-    bool operator==(const Player& other) const {
-        return this->score == other.score && this->index == other.index;
-    }
 };
 
-template <class Iterator, class Predicate>
+template <typename Iterator>
+struct PlayersTeam {
+    Iterator first;
+    Iterator last;
+};
+
+template <typename Iterator, typename Predicate>
 Iterator Partition(Iterator first, Iterator last, Predicate pred) {
     Iterator split_it = first;
 
@@ -43,28 +37,24 @@ Iterator Partition(Iterator first, Iterator last, Predicate pred) {
     return split_it;
 }
 
-template <typename Iterator>
-Iterator SelectPivot(Iterator first, Iterator last) {
+template <typename Iterator, typename Generator>
+Iterator SelectPivot(Iterator first, Iterator last, Generator generator) {
     size_t distance = std::distance(first, last);
-    if (distance <= 1) {
-        return first;
-    }
 
-    const int kRandomSeed = 667;
-    std::mt19937 rng(kRandomSeed);
     std::uniform_int_distribution<int> distrib(0, distance - 1);
 
-    const int kPivotIndex = distrib(rng);
+    const int kPivotIndex = distrib(generator);
     return first + kPivotIndex;
 }
 
-template <typename Iterator, typename Comparator>
-void QuickSort(Iterator first, Iterator last, Comparator comparator) {
+template <typename Iterator, typename Comparator, typename Generator>
+void QuickSort(Iterator first, Iterator last, Comparator comparator,
+               Generator generator) {
     if (std::distance(first, last) <= 1) {
         return;
     }
 
-    const auto kPivot = *SelectPivot(first, last);
+    const auto kPivot = *SelectPivot(first, last, generator);
 
     const Iterator kEqualIterator =
         Partition(first, last, [kPivot, comparator](const auto& elem) {
@@ -80,6 +70,14 @@ void QuickSort(Iterator first, Iterator last, Comparator comparator) {
     QuickSort(kGreaterIterator, last, comparator);
 }
 
+template <typename Iterator, typename Comparator>
+void QuickSort(Iterator first, Iterator last, Comparator comparator) {
+    const int kRandomState = 667;
+    std::mt19937 random_generator(kRandomState);
+
+    QuickSort(first, last, comparator, random_generator);
+}
+
 template <typename Iterator>
 std::vector<Player> BuildMostEffectiveSolidaryTeam(Iterator first,
                                                    Iterator last) {
@@ -93,8 +91,7 @@ std::vector<Player> BuildMostEffectiveSolidaryTeam(Iterator first,
     int64_t best_summary_score = 0;
     int64_t current_summary_score = 0;
 
-    Iterator best_first = first;
-    Iterator best_last = first;
+    PlayersTeam<Iterator> best_team{first, std::next(first)};
 
     Iterator current_first = first;
     for (Iterator current_last = first; current_last != last; ++current_last) {
@@ -106,19 +103,14 @@ std::vector<Player> BuildMostEffectiveSolidaryTeam(Iterator first,
             ++current_first;
         }
 
-        if (current_summary_score >= best_summary_score) {
+        if (current_summary_score > best_summary_score) {
             best_summary_score = current_summary_score;
-            best_first = current_first;
-            best_last = current_last;
+            best_team.first = current_first;
+            best_team.last = std::next(current_last);
         }
     }
 
-    std::vector<Player> answer;
-    for (auto current_it = best_first; current_it <= best_last; ++current_it) {
-        answer.push_back(*current_it);
-    }
-
-    return answer;
+    return std::vector<Player>(best_team.first, best_team.last);
 }
 
 std::vector<Player> InputPlayersVector() {
@@ -138,20 +130,23 @@ int main() {
 
     std::vector<Player> players = InputPlayersVector();
 
+    const int kRandomState = 667;
+    std::mt19937 random_generator(kRandomState);
+
     QuickSort(players.begin(), players.end(), Player::CompByScore);
 
-    std::vector<Player> best_players =
+    std::vector<Player> best_team =
         BuildMostEffectiveSolidaryTeam(players.begin(), players.end());
 
     int64_t summary_score = 0;
-    for (const auto& player : best_players) {
+    for (const auto& player : best_team) {
         summary_score += player.score;
     }
 
-    QuickSort(best_players.begin(), best_players.end(), Player::CompByIndex);
+    QuickSort(best_team.begin(), best_team.end(), Player::CompByIndex);
 
     std::cout << summary_score << std::endl;
-    for (const auto& player : best_players) {
+    for (const auto& player : best_team) {
         std::cout << player.index << ' ';
     }
 
