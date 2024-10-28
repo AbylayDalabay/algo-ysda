744604b322776c4e3f73146085c4897c45d81e66
AbylayDalabay <dalabayabilay@gmail.com>
2024-10-28 22:07:16 +0500

    working fixed_set

--- contest_3/fixed_set.h
+++ contest_3/fixed_set.h
@@ -2,41 +2,47 @@
 #include <cstdint>
 #include <functional>
 #include <iostream>
+#include <limits>
 #include <optional>
 #include <random>
-#include <set>
 #include <vector>
 
 class FixedSet {
 private:
-    const int kPrimeMod = 1e9 + 7;
-    const int kCoefCollisions = 5;
-
     int size_;
 
-    std::optional<std::function<int(int)>> main_hash_func_;
-    std::vector<std::optional<std::function<int(int)>>> bucket_hash_func_;
+    using OptionalFunction = std::optional<std::function<int(int)>>;
 
-    std::vector<std::vector<int>> bucket_collisions_;
-    std::vector<std::vector<std::optional<int>>> bucket_hash_tables_;
+    OptionalFunction main_hash_func_;
+    std::vector<OptionalFunction> bucket_hash_func_;
 
+    std::vector<std::vector<std::optional<int>>> bucket_hash_table_;
+
+    static const int64_t kInf = std::numeric_limits<int64_t>::max();
+    static const int kPrimeMod = 1e9 + 7;
+    static const int kCoefCollision = 5;
+
+public:
     static int64_t CountCollisions(const std::vector<int>& array,
-                                   const std::function<int(int)>& hash_function,
+                                   const OptionalFunction& current_hash_func,
                                    int hash_table_size) {
-        std::vector<int> collisions_count(hash_table_size, 0);
+        if (!current_hash_func.has_value()) {
+            return kInf;
+        }
+        std::vector<int> collisions_size(hash_table_size, 0);
         for (auto x : array) {
-            ++collisions_count[hash_function(x)];
+            ++collisions_size[current_hash_func.value()(x)];
         }
 
         int64_t collisions_sum = 0;
-        for (auto x : collisions_count) {
-            collisions_sum += (static_cast<int64_t>(x) * (x - 1)) / 2;
+        for (auto x : collisions_size) {
+            collisions_sum += static_cast<int64_t>(x) * (x - 1) / 2;
         }
 
         return collisions_sum;
     }
 
-    int SelectRandom() const {
+    static int SelectRandom() {
         std::random_device rd;
         std::mt19937 random_generator(rd());
         std::uniform_int_distribution<int> distrib(0, kPrimeMod - 1);
@@ -44,12 +50,14 @@ private:
         return distrib(random_generator);
     }
 
-    std::optional<std::function<int(int)>> SelectPerfectHashFunc(
-        const std::vector<int>& array, int hash_table_size,
-        int64_t collision_limit) {
+    static OptionalFunction SelectPerfectHashFunc(const std::vector<int>& array,
+                                                  int hash_table_size,
+                                                  int64_t collision_limit) {
         if (hash_table_size == 0) {
-            return std::nullopt;
+            return OptionalFunction(std::nullopt);
         }
+
+        int epoch_count = 0;
         while (true) {
             int a_param = SelectRandom();
             int b_param = SelectRandom();
@@ -62,68 +70,88 @@ private:
                        hash_table_size;
             };
 
+            ++epoch_count;
             if (CountCollisions(array, current_hash_func, hash_table_size) <=
                 collision_limit) {
-                return current_hash_func;
+                std::cout << "Converged after: " << epoch_count << std::endl;
+                return OptionalFunction(current_hash_func);
             }
         }
     }
 
-public:
     FixedSet() {}
     void Initialize(const std::vector<int>& numbers) {
         size_ = numbers.size();
-        main_hash_func_ = std::nullopt;
+        main_hash_func_ =
+            SelectPerfectHashFunc(numbers, size_, kCoefCollision * size_);
 
-        if (size_ == 0) {
+        if (!main_hash_func_.has_value()) {
             return;
         }
 
-        bucket_collisions_.resize(size_);
-        bucket_hash_tables_.resize(size_);
-        bucket_hash_func_.resize(size_);
+        std::cout << "main_hash completed" << std::endl;
 
-        main_hash_func_ = SelectPerfectHashFunc(
-            numbers, numbers.size(), kCoefCollisions * numbers.size());
-
+        std::vector<std::vector<int>> buckets(size_);
         for (auto x : numbers) {
-            int current_hash = main_hash_func_.value()(x);
-            bucket_collisions_[current_hash].push_back(x);
+            int hash_value = main_hash_func_.value()(x);
+            buckets[hash_value].push_back(x);
         }
+
+        std::cout << "buckets: " << std::endl;
+        for (auto bucket : buckets) {
+            std::cout << "bucket: ";
+            for (auto x : bucket) {
+                std::cout << x << ' ';
+            }
+            std::cout << std::endl;
+        }
+
+        bucket_hash_func_.assign(size_, std::nullopt);
+        bucket_hash_table_.resize(size_);
 
         for (int index = 0; index < size_; ++index) {
-            if (bucket_collisions_[index].empty()) {
+            int current_bucket_size = buckets[index].size();
+            int64_t current_bucket_hash_table_size =
+                static_cast<int64_t>(current_bucket_size) * current_bucket_size;
+
+            std::cout << index << " started" << std::endl;
+
+            auto hui = SelectPerfectHashFunc(buckets[index],
+                                             current_bucket_hash_table_size, 0);
+
+            std::cout << index << " returned" << std::endl;
+
+            bucket_hash_func_[index] = hui;
+
+            std::cout << index << " ended" << std::endl;
+            // std::cout << "bucket has hash func: "
+            //           << (bucket_hash_func_[index].has_value() ? "YES" :
+            //           "NO")
+            //           << std::endl;
+
+            if (!bucket_hash_func_[index].has_value()) {
                 continue;
             }
-            int current_bucket_table_size = bucket_collisions_[index].size() *
-                                            bucket_collisions_[index].size();
 
-            bucket_hash_func_[index] = SelectPerfectHashFunc(
-                bucket_collisions_[index], current_bucket_table_size, 0);
+            bucket_hash_table_[index].assign(current_bucket_hash_table_size,
+                                             std::nullopt);
 
-            bucket_hash_tables_[index].assign(current_bucket_table_size,
-                                              std::nullopt);
+            for (auto x : buckets[index]) {
+                int hash_value = bucket_hash_func_[index].value()(x);
 
-            for (auto x : bucket_collisions_[index]) {
-                bucket_hash_tables_[index]
-                                   [bucket_hash_func_[index].value()(x)] = x;
+                bucket_hash_table_[index][hash_value] = x;
             }
         }
-    }
-    bool Contains(int number) const {
-        int bucket_index = main_hash_func_.value()(number);
-        if (bucket_hash_tables_[bucket_index].empty()) {
-            return false;
+
+        std::cout << "Hash table" << std::endl;
+        for (auto bucket : bucket_hash_table_) {
+            for (auto x : bucket) {
+                if (x.has_value()) {
+                    std::cout << x.value() << ' ';
+                }
+            }
+            std::cout << std::endl;
         }
-        int bucket_value = bucket_hash_func_[bucket_index].value()(number);
-
-        std::optional<int> hash_optional_value =
-            bucket_hash_tables_[bucket_index][bucket_value];
-
-        if (hash_optional_value.has_value()) {
-            return hash_optional_value.value() == number;
-        }
-
-        return false;
     }
+    bool Contains(int number) const;
 };
