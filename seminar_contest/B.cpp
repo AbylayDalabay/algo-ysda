9ca2e67a0afc8b2b7c66eba9118b11408341da98
AbylayDalabay <dalabayabilay@gmail.com>
2024-11-20 00:40:52 +0500

    B.cpp seminar passed

--- seminar_contest/B.cpp
+++ seminar_contest/B.cpp
@@ -1,91 +1,101 @@
+#include <cstddef>
 #include <cstdint>
 #include <iostream>
 #include <vector>
 
-int64_t CountBiggerSegments(const std::vector<int64_t>& left,
-                            const std::vector<int64_t>& right,
-                            int64_t threshold) {
-    int64_t answer = 0;
-    int left_index = 0;
-    for (int right_index = 0; right_index < right.size(); ++right_index) {
-        while (left_index < left.size() &&
-               right[right_index] - left[left_index] < threshold) {
-            ++left_index;
-        }
-        answer += (left.size() - left_index);
-    }
-    return answer;
-}
+template <typename T>
+std::vector<T> Merge(const std::vector<T>& left, const std::vector<T>& right) {
+    std::vector<T> merge;
 
-std::vector<int64_t> Merge(const std::vector<int64_t>& left,
-                           const std::vector<int64_t>& right) {
-    std::vector<int64_t> merge;
-    int left_index = 0;
-    int right_index = 0;
+    size_t ptr_left = 0;
+    size_t ptr_right = 0;
 
-    while (left_index < left.size() && right_index < right.size()) {
-        if (left[left_index] <= right[right_index]) {
-            merge.push_back(left[left_index]);
-            ++left_index;
+    while (ptr_left < left.size() && ptr_right < right.size()) {
+        if (left[ptr_left] <= right[ptr_right]) {
+            merge.push_back(left[ptr_left]);
+            ++ptr_left;
         } else {
-            merge.push_back(right[right_index]);
-            ++right_index;
+            merge.push_back(right[ptr_right]);
+            ++ptr_right;
         }
     }
 
-    while (left_index < left.size()) {
-        merge.push_back(left[left_index]);
-        ++left_index;
+    while (ptr_left < left.size()) {
+        merge.push_back(left[ptr_left]);
+        ++ptr_left;
     }
 
-    while (right_index < right.size()) {
-        merge.push_back(right[right_index]);
-        ++right_index;
+    while (ptr_right < right.size()) {
+        merge.push_back(right[ptr_right]);
+        ++ptr_right;
     }
 
     return merge;
 }
 
-int64_t MergeSortCount(std::vector<int64_t> array, int64_t threshold) {
-    if (array.empty()) {
-        return 0;
-    }
-    if (array.size() == 1) {
-        return (array[0] >= threshold ? 1 : 0);
+template <typename T>
+std::vector<T> MergeSort(std::vector<T> array, T threshold, int64_t& answer) {
+    if (array.size() <= 1) {
+        return array;
     }
 
-    int64_t answer = 0;
+    size_t pivot_index = array.size() / 2;
 
-    std::vector<int64_t> left;
-    std::vector<int64_t> right;
+    std::vector<T> left;
+    std::vector<T> right;
 
-    int pivot_index = array.size() / 2;
-
-    for (int index = 0; index < pivot_index; ++index) {
+    for (size_t index = 0; index < pivot_index; ++index) {
         left.push_back(array[index]);
     }
-    for (int index = pivot_index; index < array.size(); ++index) {
+    for (size_t index = pivot_index; index < array.size(); ++index) {
         right.push_back(array[index]);
     }
 
-    left = MergeSortCount(left, threshold);
-    right = MergeSortCount(right, threshold);
+    left = MergeSort(left, threshold, answer);
+    right = MergeSort(right, threshold, answer);
+
+    size_t ptr_right = 0;
+    for (auto current_left : left) {
+        while (ptr_right < right.size() &&
+               right[ptr_right] < current_left + threshold) {
+            ++ptr_right;
+        }
+
+        answer += (right.size() - ptr_right);
+    }
+
+    return Merge(left, right);
+}
+
+int64_t CountNotLessSegments(const std::vector<int64_t>& array,
+                             int64_t threshold) {
+    int64_t answer = 0;
+    MergeSort(array, threshold, answer);
+    return answer;
 }
 
 int main() {
     int array_size;
     std::cin >> array_size;
+
     std::vector<int64_t> array(array_size);
     for (auto& x : array) {
         std::cin >> x;
     }
 
     std::vector<int64_t> prefix_sum(array_size + 1, 0);
-    for (int index = 0; index < array_size; ++index) {
-        prefix_sum[index + 1] = prefix_sum[index] + array[index];
+    for (int index = 1; index <= array_size; ++index) {
+        prefix_sum[index] = prefix_sum[index - 1] + array[index - 1];
     }
 
-    int64_t min_value;
-    int64_t max_value;
-    std::cin >> min_value >> max_value;
+    int64_t min_bound;
+    int64_t max_bound;
+    std::cin >> min_bound >> max_bound;
+
+    int64_t bigger_max_bound_count =
+        CountNotLessSegments(prefix_sum, max_bound + 1);
+    int64_t bigger_min_bound_count =
+        CountNotLessSegments(prefix_sum, min_bound);
+
+    std::cout << bigger_min_bound_count - bigger_max_bound_count << std::endl;
 }
